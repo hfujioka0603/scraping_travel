ad3a526671fede147462b7bb4bd2ba38773b3218
hfujioka0603 <62428534+hfujioka0603@users.noreply.github.com>
2020-06-04 21:27:09 +0900

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -1,32 +1,8 @@
-"""目録
-概要：WEBスクレイピングによる、旅行サイト各社のキャンペーン情報の収集
-目的：旅行業界におけるトレンドや需要期を定量的に把握することにより、クライアントへの施策提案材料とする。
-対象サイト：トラベラーズナビ様
-対象ページ：https://travelersnavi.com/coupon/category/waribiki/
-対象ページ：最新1〜2ページ
-対象箇所：キャンペーン見出しのタイトル・日付部分
-
-補足：
-robot.txtの有無は確認済み。
-その上で対象サイトへのサーバー配慮としてページネーションインターバルは2秒としている。
-"""
-
 from selenium import webdriver
 import chromedriver_binary
 import time
 import pandas as pd
 import re
-
-"""importについての補足/注意点
-・selenium、chromedriver、pandasはpipインストールが必要。
-　その際、今回はpython3で実行するため、「pip3」にてインストールを行った。
-　参考：
-　https://www.haya-programming.com/entry/2018/09/09/202711#%E3%81%AA%E3%81%9C%E3%81%93%E3%81%AE%E5%AF%BE%E5%87%A6%E3%81%A7%E8%89%AF%E3%81%84%E3%81%AE
-・chromedriverはブラウザのバージョンに合わせてpipする。
-　参考：
-　https://chromedriver.chromium.org/downloads/version-selection
-　https://qiita.com/hanzawak/items/2ab4d2a333d6be6ac760
-"""
 
 TITLE_LIST = []
 DAY_LIST = []
